b8fc1aba7f1b832059042edf4fcd11b91ef8d527
jeremyc009 <jeremyc009@deltalearns.ca>
2020-10-08 11:05:16 -0700

    update

--- problem2.py
+++ problem2.py
@@ -7,10 +7,16 @@ Return value is the distance between the (x,y) coordinates.
 Note that the coordinates should be signed (positive or negative) floats
 (2 points)
 """
-import math
-def distance(a,b,c,d):
+
+def distance(coord1, coord2):
+    a=coord1[0]
+    b=coord1[1]
+    c=coord2[0]
+    d=coord2[1]
     x=a-c
     y=b-d
     dist=(x**2 + y**2)**(1/2)
+    dist=round(dist,3)
     return dist
-
+num=distance((2,3),(5,6))
+print(num)
